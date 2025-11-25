2e9b9598c863d179de6f7f28fda3ea9819a31490
abdelllatif <haissouneabdellatif749@gmail.com>
2025-11-25 23:08:17 +0100

    here i add the PermissionChecker logic pour verifier les permission et auth

--- src/main/java/com/SmartShop/SmartShop/utils/PermissionChecker.java
+++ src/main/java/com/SmartShop/SmartShop/utils/PermissionChecker.java
@@ -1,4 +1,27 @@
 package com.SmartShop.SmartShop.utils;
 
+import com.SmartShop.SmartShop.enums.UserRole;
+import com.SmartShop.SmartShop.model.User;
+import jakarta.servlet.http.HttpSession;
+
 public class PermissionChecker {
+    public static boolean hasRole(HttpSession session, String role){
+        User user =(User) session.getAttribute("user");
+        if (user == null) return false;
+        return user.getRole().equals(role);
+    }
+    public static boolean canPerform(HttpSession session, String role, String action) {
+        User user = (User) session.getAttribute("USER");
+        if (user == null) return false;
+
+        if(user.getRole().equals(UserRole.ADMIN.toString())) {
+            return true;
+        }
+
+        if(user.getRole().equals(UserRole.CLIENT.toString())) {
+            return action.equalsIgnoreCase("READ");
+        }
+
+        return false;
+    }
 }
