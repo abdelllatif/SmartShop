a2d80de6ad08766d180fe0f33bc1e6a90aef1abf
abdelllatif <haissouneabdellatif749@gmail.com>
2025-11-25 10:59:59 +0100

    here i add the entity of client based on the role of the user=CLIENT

--- src/main/java/com/SmartShop/SmartShop/model/Client.java
+++ src/main/java/com/SmartShop/SmartShop/model/Client.java
@@ -1,4 +1,23 @@
 package com.SmartShop.SmartShop.model;
 
+
+import com.SmartShop.SmartShop.enums.CustomerTier;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private CustomerTier niveauFidelite;
+    @OneToOne(mappedBy = "client")
+    private User user;
 }
