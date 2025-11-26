5a5e73e11cbbc1842a53a5d53a2079dd2d09ed38
abdelllatif <haissouneabdellatif749@gmail.com>
2025-11-26 12:02:48 +0100

    add the UserMapper

---
+++ src/main/java/com/SmartShop/SmartShop/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.SmartShop.SmartShop.mapper;
+
+import com.SmartShop.SmartShop.dto.*;
+import com.SmartShop.SmartShop.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    User toUser(UserRequest dto);
+
+    UserResponse toUserResponse(User user);
+}
