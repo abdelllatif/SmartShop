db2d8be114efb42cbdb436c0f240d2dcbb05aefa
abdelllatif <haissouneabdellatif749@gmail.com>
2025-11-26 16:39:10 +0100

    add ProductServiceImpl

---
+++ src/main/java/com/SmartShop/SmartShop/service/impl/ProductServiceImpl.java
@@ -0,0 +1,48 @@
+package com.SmartShop.SmartShop.service.impl;
+
+import com.SmartShop.SmartShop.model.Product;
+import com.SmartShop.SmartShop.repository.ProductRepository;
+import com.SmartShop.SmartShop.service.ProductService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Optional<Product> getProductById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    @Override
+    public Product updateProduct(Long id, Product product) {
+        Product existing = productRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product not found"));
+        existing.setName(product.getName());
+        existing.setPrixUnitaire(product.getPrixUnitaire());
+        return productRepository.save(existing);
+    }
+
+    @Override
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
+}
