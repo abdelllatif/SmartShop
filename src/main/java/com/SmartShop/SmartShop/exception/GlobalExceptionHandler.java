f75bdff3b680a3279376b0e0f7d9bb71f946bffe
abdelllatif <haissouneabdellatif749@gmail.com>
2025-11-26 11:01:09 +0100

    Adding a NotFoundException to  GlobalExceptionHandler

--- src/main/java/com/SmartShop/SmartShop/exception/GlobalExceptionHandler.java
+++ src/main/java/com/SmartShop/SmartShop/exception/GlobalExceptionHandler.java
@@ -12,5 +12,8 @@ public class GlobalExceptionHandler {
     public ResponseEntity handleBadRequestException(BadRequestException e) {
         return ResponseEntity.status(400).body(e.getMessage());
     }
-
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity handleNotFoundException(NotFoundException e){
+        return ResponseEntity.status(404).body(e.getMessage());
+    }
 }
