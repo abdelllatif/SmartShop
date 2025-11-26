f47a8193cf46ae149765afdeafdf8b5361588f13
abdelllatif <haissouneabdellatif749@gmail.com>
2025-11-26 11:49:34 +0100

    add the UserResponse dto

---
+++ src/main/java/com/SmartShop/SmartShop/dto/UserResponse.java
@@ -0,0 +1,12 @@
+package com.SmartShop.SmartShop.dto;
+
+import com.SmartShop.SmartShop.enums.UserRole;
+import lombok.Data;
+
+@Data
+public class UserResponse {
+    private Long id;
+    private String username;
+    private String email;
+    private UserRole role;
+}
