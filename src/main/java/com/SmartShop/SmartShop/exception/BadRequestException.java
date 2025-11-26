8fa082bc482e62c39ee5cd397d1dbe6e9ac615f2
abdelllatif <haissouneabdellatif749@gmail.com>
2025-11-26 10:16:14 +0100

    Adding the first class of exeptions BadRequestException

---
+++ src/main/java/com/SmartShop/SmartShop/exception/BadRequestException.java
@@ -0,0 +1,8 @@
+package com.SmartShop.SmartShop.exception;
+
+public class BadRequestException extends RuntimeException{
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
