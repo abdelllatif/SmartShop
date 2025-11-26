4661476ba8c02dd947bde656c454b5f674ff08f7
abdelllatif <haissouneabdellatif749@gmail.com>
2025-11-26 10:29:45 +0100

    Adding a class of exeptions of InternalServerErrorException

---
+++ src/main/java/com/SmartShop/SmartShop/exception/InternalServerErrorException.java
@@ -0,0 +1,7 @@
+package com.SmartShop.SmartShop.exception;
+
+public class InternalServerErrorException extends RuntimeException {
+  public InternalServerErrorException(String message) {
+    super(message);
+  }
+}
