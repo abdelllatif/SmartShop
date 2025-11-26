30da418f71c57f02fcac0dcd99d98841345fd677
abdelllatif <haissouneabdellatif749@gmail.com>
2025-11-26 11:00:41 +0100

    Adding a BadRequestException to  GlobalExceptionHandler

--- src/main/java/com/SmartShop/SmartShop/exception/GlobalExceptionHandler.java
+++ src/main/java/com/SmartShop/SmartShop/exception/GlobalExceptionHandler.java
@@ -1,4 +1,16 @@
 package com.SmartShop.SmartShop.exception;
 
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity handleBadRequestException(BadRequestException e) {
+        return ResponseEntity.status(400).body(e.getMessage());
+    }
+
 }
