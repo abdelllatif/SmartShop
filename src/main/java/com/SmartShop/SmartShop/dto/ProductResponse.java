6e3734457cb48c6292f4ab2e1bf50c7c7c870f07
abdelllatif <haissouneabdellatif749@gmail.com>
2025-11-27 02:04:26 +0100

    add the ProductResponse

--- src/main/java/com/SmartShop/SmartShop/dto/ProductResponse.java
+++ src/main/java/com/SmartShop/SmartShop/dto/ProductResponse.java
@@ -1,4 +1,14 @@
 package com.SmartShop.SmartShop.dto;
 
+import com.SmartShop.SmartShop.enums.ProductStatus;
+import lombok.Data;
+
+@Data
 public class ProductResponse {
+
+    private Long id;
+    private String name;
+    private double prixUnitaire;
+    private int stockDisponible;
+    private ProductStatus status;
 }
