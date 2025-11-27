27f3e482ddb7f64a559cb7fa47e83c45f3e6ca16
abdelllatif <haissouneabdellatif749@gmail.com>
2025-11-27 17:17:53 +0100

    add the PaymentResponse  dto

--- src/main/java/com/SmartShop/SmartShop/dto/PaymentResponse.java
+++ src/main/java/com/SmartShop/SmartShop/dto/PaymentResponse.java
@@ -1,4 +1,17 @@
 package com.SmartShop.SmartShop.dto;
 
+import com.SmartShop.SmartShop.enums.PaymentStatus;
+import com.SmartShop.SmartShop.enums.PaymentType;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
 public class PaymentResponse {
+    private Long id;
+    private Long commandeId;
+    private double montant;
+    private PaymentType typePaiement;
+    private LocalDate datePaiement;
+    private PaymentStatus statutPaiement;
 }
