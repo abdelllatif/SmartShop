65a5a53882daeabf91c004ff3cd6a3e2fb0e4c5c
abdelllatif <haissouneabdellatif749@gmail.com>
2025-11-27 01:55:55 +0100

    add the Commandemapper

--- src/main/java/com/SmartShop/SmartShop/mapper/CommandeMapper.java
+++ src/main/java/com/SmartShop/SmartShop/mapper/CommandeMapper.java
@@ -1,4 +1,21 @@
 package com.SmartShop.SmartShop.mapper;
 
-public class CommandeMapper {
+import com.SmartShop.SmartShop.dto.*;
+import com.SmartShop.SmartShop.model.Commande;
+import com.SmartShop.SmartShop.model.OrderItem;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface CommandeMapper {
+
+
+    @Mapping(source = "client.id", target = "clientId")
+    CommandeResponse toCommandeResponse(Commande commande);
+
+    @Mapping(source = "product.id", target = "productId")
+    @Mapping(source = "prixUnitaire", target = "prixUnitaire")
+    @Mapping(source = "total", target = "total")
+    OrderItemResponse toOrderItemResponse(OrderItem orderItem);
 }
