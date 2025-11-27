4293e58de98f3b1e61c2ff726d3243cb9e464003
abdelllatif <haissouneabdellatif749@gmail.com>
2025-11-27 02:03:08 +0100

    add the ProductRequest

--- src/main/java/com/SmartShop/SmartShop/dto/ProductRequest.java
+++ src/main/java/com/SmartShop/SmartShop/dto/ProductRequest.java
@@ -1,4 +1,23 @@
 package com.SmartShop.SmartShop.dto;
 
+import com.SmartShop.SmartShop.enums.ProductStatus;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
 public class ProductRequest {
+
+    @NotBlank(message = "Product name is required")
+    private String name;
+
+    @Min(value = 0, message = "Unit price must be at least 0")
+    private double prixUnitaire;
+
+    @Min(value = 0, message = "Stock must be at least 0")
+    private int stockDisponible;
+
+    @NotNull(message = "Status is required")
+    private ProductStatus status;
 }
