f97187254a3519a415f68b688eb5ab538a74be7d
abdelllatif <haissouneabdellatif749@gmail.com>
2025-11-27 10:11:19 +0100

    add the PromoCodeServiceImpl

--- src/main/java/com/SmartShop/SmartShop/repository/PromoCodeRepository.java
+++ src/main/java/com/SmartShop/SmartShop/repository/PromoCodeRepository.java
@@ -3,9 +3,16 @@ package com.SmartShop.SmartShop.repository;
 import com.SmartShop.SmartShop.model.PromoCode;
 import com.SmartShop.SmartShop.enums.PromoStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.Optional;
 
 public interface PromoCodeRepository extends JpaRepository<PromoCode, Long> {
 
     Optional<PromoCode> findByCodeAndStatus(String code, PromoStatus status);
+
+    @Modifying
+    @Query("UPDATE PromoCode p SET p.status = :status WHERE p.id = :id")
+    void updateStatus(Long id, PromoStatus status);
 }
--- src/main/java/com/SmartShop/SmartShop/service/impl/PromoCodeServiceImpl.java
+++ src/main/java/com/SmartShop/SmartShop/service/impl/PromoCodeServiceImpl.java
@@ -1,4 +1,37 @@
 package com.SmartShop.SmartShop.service.impl;
 
-public class PromoCodeServiceImpl {
+import com.SmartShop.SmartShop.model.PromoCode;
+import com.SmartShop.SmartShop.model.Client;
+import com.SmartShop.SmartShop.enums.PromoStatus;
+import com.SmartShop.SmartShop.repository.PromoCodeRepository;
+import com.SmartShop.SmartShop.service.PromoCodeService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class PromoCodeServiceImpl implements PromoCodeService {
+
+    private final PromoCodeRepository promoCodeRepository;
+
+    public PromoCodeServiceImpl(PromoCodeRepository promoCodeRepository) {
+        this.promoCodeRepository = promoCodeRepository;
+    }
+
+    @Override
+    public PromoCode createPromoCode(Client client, String code) {
+        PromoCode promoCode = PromoCode.builder()
+                .client(client)
+                .code(code)
+                .status(PromoStatus.ACTIVE)
+                .used(false)
+                .build();
+        return promoCodeRepository.save(promoCode);
+    }
+
+    @Override
+    @Transactional
+    public void expirePromoCode(PromoCode promoCode) {
+        promoCodeRepository.updateStatus(promoCode.getId(), PromoStatus.EXPIRE);
+    }
+
 }
