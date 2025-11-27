2e1a196dd8df07f5721ac632889f785fe3134d59
abdelllatif <haissouneabdellatif749@gmail.com>
2025-11-27 01:50:53 +0100

    add the CommandeResponse

--- src/main/java/com/SmartShop/SmartShop/dto/CommandeResponse.java
+++ src/main/java/com/SmartShop/SmartShop/dto/CommandeResponse.java
@@ -1,4 +1,23 @@
 package com.SmartShop.SmartShop.dto;
 
+import com.SmartShop.SmartShop.enums.OrderStatus;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
 public class CommandeResponse {
+
+    private Long id;
+    private Long clientId;
+    private List<OrderItemResponse> listeArticles;
+    private LocalDate date;
+    private double sousTotal;
+    private double remise;
+    private double tva;
+    private double total;
+    private String codePromo;
+    private OrderStatus statut;
+    private double montantRestant;
 }
