ee0eb6c53bbf1c2d16e60f1bfbcd686883fb332c
abdelllatif <haissouneabdellatif749@gmail.com>
2025-11-27 10:00:30 +0100

    add the entity of PromoCode

--- src/main/java/com/SmartShop/SmartShop/model/PromoCode.java
+++ src/main/java/com/SmartShop/SmartShop/model/PromoCode.java
@@ -1,4 +1,27 @@
 package com.SmartShop.SmartShop.model;
 
+import com.SmartShop.SmartShop.enums.PromoStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class PromoCode {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String code;
+
+    @ManyToOne
+    private Client client;
+
+    @Enumerated(EnumType.STRING)
+    private PromoStatus status = PromoStatus.ACTIVE;
+
+    private boolean used = false;
 }
