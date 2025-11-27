9454ef42efa419b8a0de8c7e679c3d4b3140020e
abdelllatif <haissouneabdellatif749@gmail.com>
2025-11-27 01:47:36 +0100

    add the CommandeRequest

--- src/main/java/com/SmartShop/SmartShop/dto/CommandeRequest.java
+++ src/main/java/com/SmartShop/SmartShop/dto/CommandeRequest.java
@@ -1,4 +1,24 @@
 package com.SmartShop.SmartShop.dto;
 
+import com.SmartShop.SmartShop.enums.OrderStatus;
+import lombok.Data;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.List;
+
+@Data
 public class CommandeRequest {
+
+    @NotNull(message = "Client is required")
+    private Long clientId;
+
+    @NotEmpty(message = "At least one order item is required")
+    private List<OrderItemRequest> listeArticles;
+
+    private double remise;
+    private double tva;
+    private String codePromo;
+
+    private OrderStatus statut;
 }
