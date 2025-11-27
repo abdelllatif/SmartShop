c5531bb85ebb7300e04950fa28b6562c7ae5a267
abdelllatif <haissouneabdellatif749@gmail.com>
2025-11-27 02:13:47 +0100

    update CommandeMapper

--- src/main/java/com/SmartShop/SmartShop/mapper/CommandeMapper.java
+++ src/main/java/com/SmartShop/SmartShop/mapper/CommandeMapper.java
@@ -5,17 +5,16 @@ import com.SmartShop.SmartShop.model.Commande;
 import com.SmartShop.SmartShop.model.OrderItem;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface CommandeMapper {
 
-
     @Mapping(source = "client.id", target = "clientId")
+    @Mapping(source = "listeArticles", target = "orderItems")
     CommandeResponse toCommandeResponse(Commande commande);
 
-    @Mapping(source = "product.id", target = "productId")
-    @Mapping(source = "prixUnitaire", target = "prixUnitaire")
+    @Mapping(source = "produit.id", target = "productId") // <-- هنا استعمل الاسم الصحيح ديال الحقل فالـ Entity
+    @Mapping(source = "produit.prixUnitaire", target = "prixUnitaire") // إذا بغيت الثمن من الـ Product
     @Mapping(source = "total", target = "total")
     OrderItemResponse toOrderItemResponse(OrderItem orderItem);
 }
