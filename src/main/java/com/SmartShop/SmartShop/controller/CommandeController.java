5c80fc64e76d2e1ca98873464eab887a3511d2e3
abdelllatif <haissouneabdellatif749@gmail.com>
2025-11-27 11:49:23 +0100

    update theconstructor to  CommandeController

--- src/main/java/com/SmartShop/SmartShop/controller/CommandeController.java
+++ src/main/java/com/SmartShop/SmartShop/controller/CommandeController.java
@@ -1,4 +1,14 @@
 package com.SmartShop.SmartShop.controller;
 
+import com.SmartShop.SmartShop.service.CommandeService;
+import jakarta.servlet.http.HttpSession;
+
 public class CommandeController {
+    private final CommandeService commandeService;
+    private final HttpSession session;
+
+    public CommandeController(CommandeService commandeService, HttpSession session) {
+        this.commandeService = commandeService;
+        this.session = session;
+    }
 }
