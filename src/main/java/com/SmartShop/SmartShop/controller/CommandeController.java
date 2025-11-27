7c28cb94511f0c2849ad3a979b550380d6eaeae3
abdelllatif <haissouneabdellatif749@gmail.com>
2025-11-27 11:50:28 +0100

    add PostMapping  to  CommandeController

--- src/main/java/com/SmartShop/SmartShop/controller/CommandeController.java
+++ src/main/java/com/SmartShop/SmartShop/controller/CommandeController.java
@@ -1,7 +1,13 @@
 package com.SmartShop.SmartShop.controller;
 
+import com.SmartShop.SmartShop.dto.CommandeRequest;
+import com.SmartShop.SmartShop.dto.CommandeResponse;
+import com.SmartShop.SmartShop.exception.ForbiddenException;
 import com.SmartShop.SmartShop.service.CommandeService;
+import com.SmartShop.SmartShop.utils.PermissionChecker;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 public class CommandeController {
     private final CommandeService commandeService;
@@ -10,5 +16,13 @@ public class CommandeController {
     public CommandeController(CommandeService commandeService, HttpSession session) {
         this.commandeService = commandeService;
         this.session = session;
+    }
+
+    @PostMapping
+    public CommandeResponse createCommande(@RequestBody CommandeRequest request) {
+        if (!PermissionChecker.canPerform(session, "CREATE")) {
+            throw new ForbiddenException("Access denied");
+        }
+        return commandeService.createCommande(request);
     }
 }
