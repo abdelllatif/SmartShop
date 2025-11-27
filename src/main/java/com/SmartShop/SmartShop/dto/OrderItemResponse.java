efed473e253d65163aba2657a6b6aa8574fea348
abdelllatif <haissouneabdellatif749@gmail.com>
2025-11-27 01:52:10 +0100

    add the OrderItemResponse  dto

--- src/main/java/com/SmartShop/SmartShop/dto/OrderItemResponse.java
+++ src/main/java/com/SmartShop/SmartShop/dto/OrderItemResponse.java
@@ -1,4 +1,12 @@
 package com.SmartShop.SmartShop.dto;
 
+import lombok.Data;
+
+@Data
 public class OrderItemResponse {
+    private Long id;
+    private Long productId;
+    private int quantity;
+    private double prixUnitaire;
+    private double total;
 }
