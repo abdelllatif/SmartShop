6580b78da5ad04cc4a20488f3fa525d806e47dcb
abdelllatif <haissouneabdellatif749@gmail.com>
2025-11-27 11:42:24 +0100

    add the class PromoCodeGenerator with the instance of the SecureRandom

--- src/main/java/com/SmartShop/SmartShop/utils/PromoCodeGenerator.java
+++ src/main/java/com/SmartShop/SmartShop/utils/PromoCodeGenerator.java
@@ -2,8 +2,14 @@ package com.SmartShop.SmartShop.utils;
 
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
+import java.security.SecureRandom;
+
 public class PromoCodeGenerator {
     private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    public static String generatePromoCode() {
+    private static final SecureRandom random= new SecureRandom();
+    public static String generatePromoCode(int lenght) {
+        StringBuffer sb =new StringBuffer((lenght));
+        for (int i = 0; i < lenght; i++) sb.append((CHARACTERS.charAt(random.nextInt(CHARACTERS.length()))));
+        return sb.toString();
     }
 }
