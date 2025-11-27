a13f324d1d3c1f0110d3a3af2a4ad5205c22ec8f
abdelllatif <haissouneabdellatif749@gmail.com>
2025-11-27 17:17:13 +0100

    add the PaymentRequest dto

--- src/main/java/com/SmartShop/SmartShop/dto/PaymentRequest.java
+++ src/main/java/com/SmartShop/SmartShop/dto/PaymentRequest.java
@@ -1,4 +1,11 @@
 package com.SmartShop.SmartShop.dto;
 
+import com.SmartShop.SmartShop.enums.PaymentType;
+import lombok.Data;
+
+@Data
 public class PaymentRequest {
+    private Long commandeId;
+    private double montant;
+    private PaymentType typePaiement;
 }
