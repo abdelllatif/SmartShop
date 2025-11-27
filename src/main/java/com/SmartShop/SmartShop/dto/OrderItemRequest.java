97dedd807976c9a3800fc8a65916ee3be4d145cd
abdelllatif <haissouneabdellatif749@gmail.com>
2025-11-27 01:48:54 +0100

    add the OrderItemRequest

--- src/main/java/com/SmartShop/SmartShop/dto/OrderItemRequest.java
+++ src/main/java/com/SmartShop/SmartShop/dto/OrderItemRequest.java
@@ -1,4 +1,15 @@
 package com.SmartShop.SmartShop.dto;
 
+import lombok.Data;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+@Data
 public class OrderItemRequest {
+    @NotNull(message = "Product ID is required")
+    private Long productId;
+
+    @Min(value = 1, message = "Quantity must be at least 1")
+    private int quantity;
 }
