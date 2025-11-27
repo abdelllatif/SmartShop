31965c01c72c529d754ab69f3919676e1dd4fa36
abdelllatif <haissouneabdellatif749@gmail.com>
2025-11-27 17:16:27 +0100

    add the PayementRepository

--- src/main/java/com/SmartShop/SmartShop/repository/PayementRepository.java
+++ src/main/java/com/SmartShop/SmartShop/repository/PayementRepository.java
@@ -1,4 +1,14 @@
 package com.SmartShop.SmartShop.repository;
 
-public class PayementRepository {
+import com.SmartShop.SmartShop.model.Payement;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PayementRepository extends JpaRepository<Payement, Long> {
+
+    List<Payement> findByCommandeId(Long commandeId);
+
 }
