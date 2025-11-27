22380684a2e44c17e7d5b75de0923d63fa228656
abdelllatif <haissouneabdellatif749@gmail.com>
2025-11-27 17:19:24 +0100

    add the PaymentService  interface methodes signaturez

--- src/main/java/com/SmartShop/SmartShop/service/PaymentService.java
+++ src/main/java/com/SmartShop/SmartShop/service/PaymentService.java
@@ -1,4 +1,14 @@
 package com.SmartShop.SmartShop.service;
 
+import com.SmartShop.SmartShop.dto.PaymentRequest;
+import com.SmartShop.SmartShop.dto.PaymentResponse;
+
+import java.util.List;
+
 public interface PaymentService {
+
+    PaymentResponse createPayment(PaymentRequest request);
+
+    List<PaymentResponse> getPaymentsByCommande(Long commandeId);
+
 }
