7de5124106b95cfbf40242981f535cb1d6c6b67d
abdelllatif <haissouneabdellatif749@gmail.com>
2025-11-28 07:41:30 +0100

    add the PaymentMapper

--- src/main/java/com/SmartShop/SmartShop/mapper/PaymentMapper.java
+++ src/main/java/com/SmartShop/SmartShop/mapper/PaymentMapper.java
@@ -1,4 +1,17 @@
 package com.SmartShop.SmartShop.mapper;
 
-public class PaymentMapper {
+import com.SmartShop.SmartShop.dto.PaymentRequest;
+import com.SmartShop.SmartShop.dto.PaymentResponse;
+import com.SmartShop.SmartShop.model.Payement;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface PaymentMapper {
+
+    @Mapping(source = "commande.id", target = "commandeId")
+    PaymentResponse toPaymentResponse(Payement payement);
+
+    @Mapping(source = "commandeId", target = "commande.id")
+    Payement toPayement(PaymentRequest request);
 }
